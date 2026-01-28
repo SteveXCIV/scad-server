3772a9cbb50301d2f52f57010cb04ceb26ac92d3
Steve Troetti <SteveXCIV@gmail.com>
2026-01-28 03:08:03 -0500

    clean up tempdir

--- services/openscad.go
+++ services/openscad.go
@@ -54,6 +54,12 @@ func (s *OpenSCADService) Export(req *models.ExportRequest) ([]byte, string, err
 		log.Printf("[OpenSCAD Export] Failed to create temp dir: %v", err)
 		return nil, "", fmt.Errorf("failed to create temp directory: %w", err)
 	}
+	defer func() {
+		if err := os.RemoveAll(tmpDir); err != nil {
+			// Log error but don't fail the operation
+			fmt.Fprintf(os.Stderr, "warning: failed to remove temp directory %s: %v\n", tmpDir, err)
+		}
+	}()
 
 	// Write SCAD content to temporary file
 	scadFile := filepath.Join(tmpDir, "input.scad")
